22149e8edde719dc5f7beee495d5772ff984a5df
nguyenvanduy <xnguyenduyx025@gmail.com>
2021-04-30 17:01:53 +0700

    nit projrct

---
+++ DOAN_Angular/client/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { url } from 'inspector';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: 
+  [
+    './app.component.css',
+  ]
+})
+export class AppComponent {
+  title = 'Angular 8 + Spring Boot 2 CRUD Tutorial';
+}
+
